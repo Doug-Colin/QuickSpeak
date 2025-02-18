75bfa88589e1782f20df53e87fe4972aa74dd407
Doug-Colin <102709219+Doug-Colin@users.noreply.github.com>
2025-02-18 01:40:10 -0500

    Added files from updated local to new pull from remote, which was created due to git init isue in local

--- src/pages/landing-page/LandingPage.tsx
+++ src/pages/landing-page/LandingPage.tsx
@@ -10,22 +10,19 @@ export default function LandingPage() {
             QuickSpeak
           </h1>
           <h2 className="text-3xl mb-3">
-            Knowing what you want to say and not being able to say it is
-            frustrating.
+          Less time trying. More time speaking.
           </h2>
+          <p className="max-w-[500px] text-muted-foreground text-xl mb-4 justify-left">
+          QuickSpeak is designed to make life easier for patients with Parkinson's and other speech challenges by helping them communicate more efficiently and effectively. 
+          </p>
           <p className="max-w-[500px] text-muted-foreground text-xl mb-4">
-          QuickSpeak is an designed to make life easier for patients with Parkinson's and other speech challenges by helping them communicate more effectively and efficiently. 
-          
-          Express yourself with ease by quickly creating questions, responses, and phrases using predictive text. Once you know what you want to say, QuickSpeak can say it for you. Choose from a collection of voices to find one that fits, adjust volume, and save your voice and commonly used phrases for later use. 
+          Express yourself with ease by quickly creating statements with predictive text, saving them for later use, and saying them in a voice of your choosing.
           </p>
           <Button asChild variant="secondary" size="lg">
             <Link to="/register" className="mt-4 mb-16">
               Try it out!
             </Link>
           </Button>
-          <h2 className="text-3xl mb-3">
-            Less time trying. More time speaking.
-          </h2>
         </section>
       </main>
     </>
